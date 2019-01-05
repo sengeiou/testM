5afdad5ad0bfedb4b70be25bce7fbc0a2d266aa9
卜沉 <17636813864@163.com>
2019-01-05 12:43:48 +0800

    头报 红铺布局

---
+++ app/src/main/java/com/qingmeng/mengmeng/adapter/NewspaperAdapter.kt
@@ -0,0 +1,11 @@
+package com.qingmeng.mengmeng.adapter
+
+/**
+ * Created by fyf on 2019/1/2
+ * 头报适配器
+ */
+
+class NewspaperAdapter()  {
+
+
+}
--- app/src/main/java/com/qingmeng/mengmeng/fragment/NewspaperFragment.kt
+++ app/src/main/java/com/qingmeng/mengmeng/fragment/NewspaperFragment.kt
@@ -5,4 +5,19 @@ import com.qingmeng.mengmeng.R
 
 class NewspaperFragment :BaseFragment(){
     override fun getLayoutId(): Int = R.layout.fragment_head_newspaper
+
+    override fun initListener() {
+        super.initListener()
+    }
+
+    override fun initObject() {
+        super.initObject()
+    }
+
+    override fun initData() {
+        super.initData()
+    }
+
+
+
 }
