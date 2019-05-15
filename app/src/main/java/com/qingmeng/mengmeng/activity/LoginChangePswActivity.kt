78811d44ec4c425e2b2041cc87f3144e669489bc
saodiseng <1318392199@qq.com>
2019-05-15 13:51:33 +0800

    配置文件整理

--- app/src/main/java/com/qingmeng/mengmeng/activity/LoginChangePswActivity.kt
+++ app/src/main/java/com/qingmeng/mengmeng/activity/LoginChangePswActivity.kt
@@ -58,7 +58,6 @@ class LoginChangePswActivity : BaseActivity() {
     //初始化Object
     override fun initObject() {
         super.initObject()
-
         //设置标题
         setHeadName(R.string.retrieve_password)
         from = intent.getIntExtra(FROM_TYPE, from)
